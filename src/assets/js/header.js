c3dcbaa03cf245daf0c67499d52835619acf1921
SBurnt <zip2004zip@gmail.com>
2021-03-21 14:04:26 +0300

    fixed click on the submenu__back

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -19,28 +19,22 @@ if (window.innerWidth <= 1024) {
 	// opening the mobile menu START
 	const btnMobMenu = document.querySelector('.js-btn-mob-menu');
 	const mobileMenu = document.querySelector('.header__nav');
+
 	btnMobMenu.addEventListener('click', function() {
 		mobileMenu.classList.toggle('active-mob');
 	});
+
+	mobileMenu.addEventListener('click', e => {
+		const mobMenuDrop = e.target.closest('.mobMenuDrop');
+		const mobMenuDropBack = e.target.closest('.submenu__back');
+
+		if (mobMenuDrop) {
+			mobMenuDrop.querySelector('.submenu').classList.add('active');
+		}
+
+		if (mobMenuDropBack) {
+			mobMenuDropBack.parentElement.classList.remove('active');
+		}
+	});
 	// opening the mobile menu END
-
-	const mobItemsSubmenu = document.querySelectorAll('.mobMenuDrop');
-	mobItemsSubmenu.forEach(function(items) {
-		items.addEventListener('click', function(e) {
-			// console.log('this ', this);
-			// console.log('items last ', e.target.lastElementChild.className);
-			// this.lastElementChild.classList.add('active');
-			e.target.lastElementChild.classList.add('active');
-		});
-	});
-
-	const mobItemsSubmenuBack = document.querySelectorAll('.submenu__back');
-	mobItemsSubmenuBack.forEach(function(items) {
-		items.addEventListener('click', function(e) {
-			// console.log('itemsBack ', e);
-			// console.log('this back', this);
-			// this.parentElement.classList.remove('active');
-			e.target.parentElement.classList.remove('active');
-		});
-	});
 }
